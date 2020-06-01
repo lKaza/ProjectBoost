f29ad1480fa9b8df71be5dee25c744e67d90f9bb
lKaza <rodrigoguzman1202@gmail.com>
2020-06-01 13:28:46 -0400

    Lesson 47-Adding Sounds

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    AudioSource m_MyAudioSource;
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        m_MyAudioSource = GetComponent<AudioSource>();
+    }
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    void ProcessInput(){
+        if(Input.GetKey(KeyCode.Space))
+        {
+           rigidBody.AddRelativeForce(Vector3.up);
+           if(!m_MyAudioSource.isPlaying){
+           m_MyAudioSource.Play(); 
+           }
+        }else{
+            m_MyAudioSource.Stop();
+        }
+        if(Input.GetKey(KeyCode.A) )
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    
+    }
+    
+}
