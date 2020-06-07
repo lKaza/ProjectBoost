e3a6008f5c666d7f6baaf53a64ee7e201263f0ed
lKaza <rodrigoguzman1202@gmail.com>
2020-06-07 19:01:52 -0400

    Lesson 60-Oscilator

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -8,68 +9,125 @@ public class Movement : MonoBehaviour
     float rcsThrust = 100f;
     [SerializeField]
     float shipThrust = 100f;
-    AudioSource m_MyAudioSource;   
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip clearLevel;
+    
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem clearLevelParticles;
+    
+    
+    AudioSource m_MyAudioSource;
     Rigidbody rigidBody;
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         m_MyAudioSource = GetComponent<AudioSource>();
     }
-  
+
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
-    }
-
-void OnCollisionEnter(Collision other) {
-    switch(other.gameObject.tag){
-
-        case "Friendly":
-            print("OK");
-               break;
-
-         default: 
-            print("dead af");
-            break;
-    }
-}
-      private void Thrust(){
-          
-           if(Input.GetKey(KeyCode.Space))
+        if (state == State.Alive)
         {
-           rigidBody.AddRelativeForce(Vector3.up*shipThrust);
-
-           if(!m_MyAudioSource.isPlaying){
-           m_MyAudioSource.Play(); 
-           }
-            }else{
-            m_MyAudioSource.Stop();
+            Thrust();
+            Rotate();
         }
+    }
 
-      }
-      void Rotate(){
-        rigidBody.freezeRotation =true; // take manual rotation
+    void OnCollisionEnter(Collision other)
+    {
+        if (state != State.Alive) { return; }
 
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+
+            case "Finish":
+                StarSucessSequence();
+                break;
+
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartDeathSequence()
+    {
+        m_MyAudioSource.Stop();
+        m_MyAudioSource.PlayOneShot(death);
+        deathParticles.Play();
+        state = State.Dying;
+        Invoke("ReloadFirstScene", 1f);
+    }
+
+    private void StarSucessSequence()
+    {     
+        clearLevelParticles.Play();
+        m_MyAudioSource.Stop();
+        m_MyAudioSource.PlayOneShot(clearLevel);
         
-        float rotationThisFrame = rcsThrust * Time.deltaTime;
-        if(Input.GetKey(KeyCode.A) )
+        state = State.Transcending;
+        Invoke("LoadNextScene", 1.5f);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void ReloadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void Thrust()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
         {
-           
-            transform.Rotate(Vector3.forward*rotationThisFrame);
+            ApplyThrust();
+
         }
-        else if(Input.GetKey(KeyCode.D))
-        {        
-            transform.Rotate(-Vector3.forward*rotationThisFrame);
+        else
+        {
+            m_MyAudioSource.Stop();
+            mainEngineParticles.Stop();
         }
-    
-        rigidBody.freezeRotation=false; //resume world rotation
+    }
+    private void ApplyThrust()
+    {
+
+        rigidBody.AddRelativeForce(Vector3.up * shipThrust);
+        if (!m_MyAudioSource.isPlaying)
+        {
+            m_MyAudioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+    void Rotate()
+    {
+
+        rigidBody.freezeRotation = true; // take manual rotation     
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //resume world rotation
     }
 
 
-    }
-    
+}
+
 
---
+++ Assets/Scripts/Oscilator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscilator : MonoBehaviour
+{
+    private Vector3 startingPos;
+    [SerializeField] Vector3 movementVector;
+
+    // to DO move from inspector
+    [Range(0,1)][SerializeField] float movementFactorY; //1 fully move 0 not moved
+
+        void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactorY;
+        transform.position = startingPos + offset;
+    }
+}
